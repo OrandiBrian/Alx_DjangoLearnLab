f308111ae6ef92b648eca14c6997810f1cae2fe7
Brian Orandi <brianorandi97@gmail.com>
2025-02-23 22:02:43 +0300

    templates

--- django-models/LibraryProject/LibraryProject/urls.py
+++ django-models/LibraryProject/LibraryProject/urls.py
@@ -19,6 +19,5 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('list_books/', include('relationship_app.urls')),
-    path('library_detail/', include('relationship_app.urls')),
+    path('', include('relationship_app.urls')),
 ]
