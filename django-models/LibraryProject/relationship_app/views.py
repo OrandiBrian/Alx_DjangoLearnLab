b3ed885511d6f0c2cc276a93a8e91f5c312ba55e
Brian Orandi <brianorandi97@gmail.com>
2025-02-23 21:23:57 +0300

    function & class views

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -5,12 +5,12 @@ from .models import Book, Library
 # Function based view
 def list_books(request):
     books = Book.objects.all()
-    return render(request, 'list_books.html', {'books': books})
+    return render(request, 'relationship_app/list_books.html', {'books': books})
 
 # class based view
 class library_detail(DetailView):
     model = Library
-    template_name = 'library_detail.html'
+    template_name = 'relationship_app/library_detail.html'
     context_object_name = 'library'
 
     def get_context_data(self, **kwargs):
