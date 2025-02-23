a1b7d5bdedbd5a0e23e22e6261d6e5b7233e93c8
Brian Orandi <brianorandi97@gmail.com>
2025-02-23 21:43:14 +0300

    Booshelf

--- Introduction_to_Django/LibraryProject/bookshelf/views.py
+++ Introduction_to_Django/LibraryProject/bookshelf/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+from .models import Book
 
-# Create your views here.
+def hello_view(request):
+    """Function-based view for the hello/ URL."""
+    return HttpResponse('Hello, Django!')
