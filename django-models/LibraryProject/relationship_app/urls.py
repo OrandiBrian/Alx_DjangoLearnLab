1c87ddba1a897763cd10b5290f6f6934b69c3a81
Brian Orandi <brianorandi97@gmail.com>
2025-03-02 22:12:29 +0300

    Django Custom Permissions

--- django-models/LibraryProject/relationship_app/urls.py
+++ django-models/LibraryProject/relationship_app/urls.py
@@ -14,7 +14,7 @@ urlpatterns = [
     path('admin-dashboard/', admin_view, name='admin_dashboard'),
     path('librarian-dashboard/', librarian_view, name='librarian_dashboard'),
     path('member-dashboard/', member_view, name='member_dashboard'),
-    path("add/", add_book, name="add_book"),
-    path("edit/<int:book_id>/", edit_book, name="edit_book"),
+    path("add_book/", add_book, name="add_book"),
+    path("edit_book/<int:book_id>/", edit_book, name="edit_book"),
     path("delete/<int:book_id>/", delete_book, name="delete_book"),
 ]
