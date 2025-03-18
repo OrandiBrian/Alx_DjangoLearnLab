bc0ac1fcc17ced29d6be7f9873702cfc0c13cca4
Brian Orandi <brianorandi97@gmail.com>
2025-03-18 18:38:48 +0300

    role based views correction

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -58,7 +58,6 @@ def logout_user(request):
 def is_admin(user):
     return user.userprofile.role == 'Admin'
 
-@login_required
 @user_passes_test(is_admin)
 def admin_view(request):
     return render(request, 'admin_dashboard.html')
@@ -67,16 +66,14 @@ def admin_view(request):
 def is_librarian(user):
     return user.userprofile.role == 'Librarian'
 
-@login_required
 @user_passes_test(is_librarian)
 def librarian_view(request):
     return render(request, 'librarian_dashboard.html')
 
 # check if user is a member
 def is_member(user):
-    return user.is_authenticated and hasattr(user, 'userprofile') and user.userprofile.role == 'Member'
+    return user.userprofile.role == 'Member'
 
-@login_required
 @user_passes_test(is_member)
 def member_view(request):
     return render(request, 'member_dashboard.html')
