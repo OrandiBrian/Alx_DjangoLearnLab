61723e885de8536f10f3aae615ab99052a1a34a4
Brian Orandi <brianorandi97@gmail.com>
2025-03-25 10:23:26 +0300

    DFR authentication

--- api_project/api/urls.py
+++ api_project/api/urls.py
@@ -1,4 +1,4 @@
-from .views import BookList, BookViewSet
+from .views import BookList, BookViewSet, UserView
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
 
@@ -9,5 +9,6 @@ router.register(r'books_all', BookViewSet, basename='book_all')
 # defining urls
 urlpatterns = [
     path('books/', BookList.as_view(), name='book-list'),
-    path('', include(router.urls))
+    path('', include(router.urls)),
+    path('token/', UserView.as_view(), name='token')
 ]
--- api_project/api/views.py
+++ api_project/api/views.py
@@ -2,6 +2,9 @@ from django.shortcuts import render
 from .serializers import BookSerializer
 from .models import Book
 from rest_framework import generics, viewsets
+from rest_framework.permissions import IsAuthenticated
+from rest_framework.views import APIView
+from rest_framework.response import Response
 
 # creating an api view
 class BookList(generics.ListAPIView):
@@ -11,3 +14,9 @@ class BookList(generics.ListAPIView):
 class BookViewSet(viewsets.ModelViewSet):
     queryset = Book.objects.all()
     serializer_class = BookSerializer
+
+class UserView(APIView):
+    permission_classes = [IsAuthenticated]
+
+    def get(self, request):
+        return Response({'message': "Welcome User, you have been authenticated."})
